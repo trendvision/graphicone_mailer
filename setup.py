f3599fa64727918e02d849b10294d4134786b9e1
Ilya Kiselev <newkis710@yandex.ru>
2020-09-08 17:17:02 +0300

    Add required packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='graphicone_mailer',
     url='https://github.com/trendvision/graphicone_mailer',
     packages=['graphicone_mailer'],
-    install_requires=['ssl', 'datetime'],
+    install_requires=['datetime'],
     version='0.1',
     license='TRV',
     description='Email sending module',
